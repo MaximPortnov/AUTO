9c77f7e8f300e383587f320b72c89ad92986c500
MaximP1henka <87613293+JeKaP1chenka@users.noreply.github.com>
2023-12-12 15:25:46 +0700

    asd

---
+++ AmonicAirLines/AmonicAirLines/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AmonicAirLines
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        private static Dictionary<string, int> loginAttempts = new Dictionary<string, int>();
+        private static Dictionary<string, DateTime> lockedUsers = new Dictionary<string, DateTime>();
+
+        public MainWindow()
+        {
+            InitializeComponent();}
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = Username.Text;
+            string password = Password.Text;
+
+            if (lockedUsers.ContainsKey(username) && lockedUsers[username] > DateTime.Now)
+            {
+                // Пользователь заблокирован, показать сообщение о времени ожидания
+                TimeSpan remainingTime = lockedUsers[username] - DateTime.Now;
+                MessageBox.Show($"Пользователь заблокирован. Повторите попытку через {remainingTime.TotalSeconds} секунд.");
+                return;
+            }
+
+
+            string responseBody = "False";
+            string apiUrl = $"http://localhost:5197/CheckUsers?login={username}&password={password}";
+            HttpClient client = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    responseBody = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при выполнении запроса: {ex.Message}");
+            }
+
+            if (responseBody == "user")
+            {
+                loginAttempts[username] = 0;
+                if (lockedUsers.ContainsKey(username))
+                {
+                    lockedUsers.Remove(username);
+                }
+                Window window = new UserWindow();
+                this.Hide();
+                window.Show();
+                
+            }
+            if (responseBody == "admin")
+            {
+                loginAttempts[username] = 0;
+                if (lockedUsers.ContainsKey(username))
+                {
+                    lockedUsers.Remove(username);
+                }
+                Window window = new AdminWindow();
+                this.Hide();
+                window.Show();
+                
+            }
+            else if (responseBody == "block")
+            {
+                MessageBox.Show("Вы заблокированы!");
+            }
+            else if (responseBody == "false")
+            {
+                if (!loginAttempts.ContainsKey(username))
+                {
+                    loginAttempts[username] = 0;
+                }
+                loginAttempts[username]++;
+                if (loginAttempts[username] >= 3)
+                {
+                    lockedUsers[username] = DateTime.Now.AddSeconds(10);
+                    MessageBox.Show("Превышен лимит попыток входа. Пользователь заблокирован на 10 секунд.");
+                    return;
+                }
+                MessageBox.Show("Неправильное имя пользователя или пароль.");
+            }
+            else if (responseBody == "error")
+            {
+                MessageBox.Show("Ошибка входа");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ AmonicAirLines/AmonicAirLines/UserWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AmonicAirLines
+{
+    /// <summary>
+    /// Логика взаимодействия для UserWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window
+    { 
+        public UserWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = "time.txt";
+            File.WriteAllText(filePath, DateTime.Now.ToString());
+
+            Window window = new MainWindow();
+            this.Close();
+            window.Show();
+        }
+    }
+}
